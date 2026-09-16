e1035147c1f8f3f98354b9f1e57210f00f8b7e32
leekc3857-svg <leekc3857@gmail.com>
2026-09-16 22:16:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, send_from_directory
+from flask import Flask, jsonify, request, send_from_directory, send_file
 import sqlite3
 import os
 from datetime import datetime, timedelta
